1efe599f0953e9cf0e0c275c3c9c82b1fb3dd6a1
Ian Armstrong <ianwesleyarmstrong@gmail.com>
2021-07-08 19:48:47 -0500

    fixed tuple logic in io

--- usda_utils/io.py
+++ usda_utils/io.py
@@ -10,6 +10,8 @@ def gather_extensions(data_dir: str, extensions: str) -> List[str]:
     # support for compressor or uncompressed .fq files
     # https://stackoverflow.com/questions/4568580/python-glob-multiple-filetypes
     filenames = []
+    if type(extensions) == str:
+        extensions = (extensions,)
     for ext in extensions:
         data_dir_name = join(data_dir, ext)
         logging.debug(data_dir_name)
